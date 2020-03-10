b4bf6a54b03b2ceb2227462d0669fc1a84427401
Emberflash26 <varun.ask@gmail.com>
2020-03-10 09:38:51 -0400

    Initial commit

---
+++ app/src/main/java/com/asurivarun/layoutapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asurivarun.layoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int[] id_array;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
